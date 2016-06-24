0651cdd59bcd5071b83e634b313e01aed0f098a2
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-23 22:49:36 -0500

    :rocket: Add BaseView and BasePresenter

---
+++ app/src/main/java/com/gerardogtn/korimagas/BasePresenter.java
@@ -0,0 +1,9 @@
+package com.gerardogtn.korimagas;
+
+/**
+ * Created by gerardogtn on 6/23/16.
+ */
+public interface BasePresenter {
+
+    void start();
+}
---
+++ app/src/main/java/com/gerardogtn/korimagas/BaseView.java
@@ -0,0 +1,9 @@
+package com.gerardogtn.korimagas;
+
+/**
+ * Created by gerardogtn on 6/23/16.
+ */
+public interface BaseView<T> {
+
+    void setPresenter(T presenter);
+}
