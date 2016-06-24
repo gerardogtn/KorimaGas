6f973e4a89bc4ba36374b1a18f28bb3baf291361
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-24 00:05:43 -0500

    :art: Change indentation to 2 spaces

--- app/src/main/java/com/gerardogtn/korimagas/data/GasStation.java
+++ app/src/main/java/com/gerardogtn/korimagas/data/GasStation.java
@@ -9,83 +9,79 @@ import java.util.Date;
  */
 public class GasStation {
 
-    private int mId = -1;
-    private boolean mHasGas = false;
-    private Date mLastUpdate = Calendar.getInstance().getTime();
-    private String mName = "";
-    private String mDirections = "";
-    private String mLastMessage = "";
+  private int mId = -1;
+  private boolean mHasGas = false;
+  private Date mLastUpdate = Calendar.getInstance().getTime();
+  private String mName = "";
+  private String mDirections = "";
+  private String mLastMessage = "";
 
-    public GasStation() {
+  public GasStation() {
 
-    }
+  }
 
-    public GasStation(GasStationResponse gasStationResponse) {
-        this.mId = gasStationResponse.getId();
-        this.mHasGas = gasStationResponse.isGasAvailable();
-        this.mLastUpdate = gasStationResponse.getLastUpdate();
-        this.mName = gasStationResponse.getName();
-        this.mDirections = gasStationResponse.getAddress();
-        this.mLastMessage = gasStationResponse.getLastMessage();
-    }
+  public GasStation(GasStationResponse gasStationResponse) {
+    this.mId = gasStationResponse.getId();
+    this.mHasGas = gasStationResponse.isGasAvailable();
+    this.mLastUpdate = gasStationResponse.getLastUpdate();
+    this.mName = gasStationResponse.getName();
+    this.mDirections = gasStationResponse.getAddress();
+    this.mLastMessage = gasStationResponse.getLastMessage();
+  }
 
-    public int getId() {
-        return mId;
-    }
+  public int getId() {
+    return mId;
+  }
 
-    public void setHasGas(boolean mHasGas) {
-        this.mHasGas = mHasGas;
-    }
+  public void setHasGas(boolean mHasGas) {
+    this.mHasGas = mHasGas;
+  }
 
-    public void setLastUpdate(Date mLastUpdate) {
-        this.mLastUpdate = mLastUpdate;
-    }
+  public void setLastUpdate(Date mLastUpdate) {
+    this.mLastUpdate = mLastUpdate;
+  }
 
-    public void setLastMessage(String mLastMessage) {
-        this.mLastMessage = mLastMessage;
-    }
+  public void setLastMessage(String mLastMessage) {
+    this.mLastMessage = mLastMessage;
+  }
 
-    public boolean hasGas() {
-        return mHasGas;
-    }
+  public boolean hasGas() {
+    return mHasGas;
+  }
 
-    public Date getLastUpdate() {
-        return mLastUpdate;
-    }
+  public Date getLastUpdate() {
+    return mLastUpdate;
+  }
 
-    public String getName() {
-        return mName;
-    }
+  public String getName() {
+    return mName;
+  }
 
-    public String getDirections() {
-        return mDirections;
-    }
+  public String getDirections() {
+    return mDirections;
+  }
 
-    public String getLastMessage() {
-        return mLastMessage;
-    }
+  public String getLastMessage() {
+    return mLastMessage;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
 
-        GasStation that = (GasStation) o;
+    GasStation that = (GasStation) o;
 
-        if (!mName.equals(that.mName)) return false;
-        return mDirections.equals(that.mDirections);
+    if (!mName.equals(that.mName)) return false;
+    return mDirections.equals(that.mDirections);
+  }
 
-    }
+  @Override public int hashCode() {
+    int result = mName.hashCode();
+    result = 31 * result + mDirections.hashCode();
+    return result;
+  }
 
-    @Override
-    public int hashCode() {
-        int result = mName.hashCode();
-        result = 31 * result + mDirections.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "GasStation with name: " + mName;
-    }
+  @Override public String toString() {
+    return "GasStation with name: " + mName;
+  }
 }
