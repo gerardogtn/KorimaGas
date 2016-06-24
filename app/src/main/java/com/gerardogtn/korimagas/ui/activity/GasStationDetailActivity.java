c59441f0809ccb5c73f5dc5c1616b1d7c05fcf55
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-24 16:57:26 -0500

    :rocket: Add empty gas station detail activity

---
+++ app/src/main/java/com/gerardogtn/korimagas/ui/activity/GasStationDetailActivity.java
@@ -0,0 +1,12 @@
+package com.gerardogtn.korimagas.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GasStationDetailActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_gas_station_detail);
+  }
+}
