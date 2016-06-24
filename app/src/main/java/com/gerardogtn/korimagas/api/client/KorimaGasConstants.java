db80891c35363c466aa4881b2e8680e1c4181efd
Gerardo Teruel <gerardogtn@gmail.com>
2016-06-24 17:36:39 -0500

    :see_no_evil: Add '/' to end of base url

--- app/src/main/java/com/gerardogtn/korimagas/api/client/KorimaGasConstants.java
+++ app/src/main/java/com/gerardogtn/korimagas/api/client/KorimaGasConstants.java
@@ -5,10 +5,10 @@ package com.gerardogtn.korimagas.api.client;
  */
 public class KorimaGasConstants {
 
-  private static final String PATH_STATION = "/station";
-  private static final String PATH_STATIONS = "/stations";
+  private static final String PATH_STATION = "station";
+  private static final String PATH_STATIONS = "stations";
 
-  public static final String BASE_URL = "http://korimagas.xyz/api";
+  public static final String BASE_URL = "http://korimagas.xyz/api/";
   public static final String STATION_URL = BASE_URL + PATH_STATION;
   public static final String STATIONS_URL = BASE_URL + PATH_STATIONS;
 }
